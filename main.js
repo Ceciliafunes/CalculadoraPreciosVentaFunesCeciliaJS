004decacbcc82107d21ea4617b8e58c7e64ffdd8
Ceciliafunes <funes.ceciliagisela@gmail.com>
2024-03-19 19:29:18 -0300

    sumando objetos, array y metodos de busqueda y filtrado al proyecto

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 // Mensaje de bienvenida
 alert("¡Bienvenido a calculator precio de venta online!");
 
+
 //Funcion para soliciar nombre y verificar el mismo
 function solicitarNombre () {
     let nombre = prompt("Por favor, ingresa tu nombre para continuar: ");
@@ -11,6 +12,7 @@ function solicitarNombre () {
         return solicitarNombre();
     } else {
         return alert ("Hola " + nombre + " puedes calcular el precio ideal para tus productos");
+        
     }
 }
 solicitarNombre ();
@@ -44,10 +46,47 @@ function resultado (){
 }
 resultado ();
 
+//objeto y funcion constructora
+
+class Costos {
+    constructor (costoFijo, costoVariable, ganancia, precioVenta) {
+        this.costoFijo = costoFijo;
+        this.costoVariable = costoVariable;
+        this.ganancia = ganancia;
+        this.precioVenta = precioVenta
+    }
+    listaCostos() {
+        console.log("Estos son los datos ingresados: \n" 
+        + "Costo fijo: " + (this.costoFijo) + "\n" +
+        "Costo variable: " + (this.costoVariable) + "\n"
+        + "Ganancia pretendida: " + (this.ganancia) + "%" + "\n" 
+        + "El precio de tu producto es: $" + (this.precioVenta));
+    }
+}
+let costos1 = new Costos (costoFijo, costoVariable, ganancia, precioVenta);
+costos1.listaCostos();
+
+//array para almacenar datos
+
+const datosIngresados = [];
+
+datosIngresados.push(new Costos(costoFijo, costoVariable, ganancia, precioVenta));
+
+console.log(datosIngresados);
 
 
 
+const guardarResultado = datosIngresados.map((el) => el.precioVenta);
+console.log(guardarResultado);
 
-
-
+const contenedorDatos = document.getElementById("listaDatos");
+datosIngresados.forEach( costos => {
+    const div = document.createElement("div");
+    div.innerHTML = `   <h3> Los datos ingresados son: </h3>
+                        <ul><li> El costo fijo es: $${costos.costoFijo}</li>
+                        <li> El costo variable es: $${costos.costoVariable}</li>
+                        <li> La ganancia pretendida es: ${costos.ganancia}%</li></ul>
+                        <p> El precio ideal para su producto segun los datos recolectados es: $${costos.precioVenta}</p>`;
+                        listaDatos.appendChild(div);
+})  
 
