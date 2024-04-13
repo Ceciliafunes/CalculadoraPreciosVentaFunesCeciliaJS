0738a3eb6cdc89b40a13551425e4af6b7e24221a
Ceciliafunes <funes.ceciliagisela@gmail.com>
2024-04-13 04:57:55 -0300

    pre entrega 3 proyecto final JS

--- main.js
+++ main.js
@@ -1,37 +1,101 @@
 
-// Mensaje de bienvenida
-alert("¡Bienvenido a calculator precio de venta online!");
-
-
-//Funcion para soliciar nombre y verificar el mismo
-function solicitarNombre () {
-    let nombre = prompt("Por favor, ingresa tu nombre para continuar: ");
-
-    if (nombre === "" || nombre === null || !isNaN(nombre)) {
-        alert ("Ingresa un nombre válido para continuar");
-        return solicitarNombre();
-    } else {
-        return alert ("Hola " + nombre + " puedes calcular el precio ideal para tus productos");
+//OBJETO
+class Costos {
+    constructor (producto, costoFijo, costoVariable, ganancia, precioVenta) {
+        this.producto = producto;
+        this.costoFijo = costoFijo;
+        this.costoVariable = costoVariable;
+        this.ganancia = ganancia;
         
     }
 }
-solicitarNombre ();
 
-//variables globales
-let costoFijo = verificarDatos("Ingresa el costo fijo de tu producto, sin el signo pesos: ");
-let costoVariable = verificarDatos("Ingresa el total de costos variables. Ej. : fletes, embalaje,etc. : ");
-let ganancia = verificarDatos("Ingresa el margen de ganancia pretendido, sin el signo %. ej.: 50");
-let precioVenta = CalculoPrecioVenta(costoFijo, costoVariable, ganancia);
+Costos.prototype.CalculoPrecioVenta = function() {
+    if (this.ganancia < 100) {
+        return (this.costoFijo + this.costoVariable) / (1- this.ganancia/100);
+    } else (this.ganancia >= 100) 
+        return ((this.costoFijo + this.costoVariable) * 2);
+}
 
 
-//funcion para validar numeros
-function verificarDatos (datos) {
-    let numero = parseFloat(prompt(datos));
+//array para guardar los datos ingresados
+let datosIngresados = [];
 
-    while (isNaN(numero) || numero < 0) {
-        numero = parseFloat(prompt("Por favor ingresa un numero válido, el mismo debe ser mayor o igual a 0: "));
+if(localStorage.getItem("ultimoDatoIngresado")){
+    ultimoDatoIngresado = JSON.parse(localStorage.getItem("costos"));
+}
+
+//funciones para manejar el envio del formulario
+
+document.getElementById("formulario").addEventListener ("submit", function(e) {
+    e.preventDefault();
+    
+    let producto = document.getElementById("producto").value;
+    let costoFijo = parseFloat(document.getElementById("costoFijo").value);
+    let costoVariable = parseFloat(document.getElementById("costoVariable").value);
+    let ganancia = parseFloat(document.getElementById("ganancia").value);
+
+
+    //validacion de datos
+    if (isNaN(costoFijo) || isNaN(costoVariable) || isNaN(ganancia) || costoFijo < 0 || costoVariable < 0 || ganancia < 0) {
+        alert("ERROR. por favor complete todos los campos. Con números mayores o iguales a 0");
+        return;
     }
-    return numero;
+
+    //objeto con los valores ingresados
+    let costos = new Costos(producto, costoFijo, costoVariable, ganancia);
+
+    let precioVenta = Math.round(costos.CalculoPrecioVenta());
+
+    //Mostrar resultado
+    document.getElementById("resultado").innerHTML = `<strong>RESULTADO: $${precioVenta}</strong>`
+
+    datosIngresados.push(new Costos (producto, costoFijo, costoVariable, ganancia));
+    console.log(datosIngresados);
+
+    //guardamos todos los datos ingresados
+    localStorage.setItem("datosIngresados", JSON.stringify(datosIngresados))
+    
+    let ultimosDatos = [];
+    ultimosDatos.push(costos);
+
+    const listaDatos = document.getElementById("listaDatos");
+
+    ultimosDatos.forEach( costos => {
+    const div = document.createElement("div");
+    div.innerHTML = `   
+                        <h4> Producto: ${costos.producto}</h4>
+                        <ul><li> El costo fijo es: $${costos.costoFijo}</li>
+                        <li> El costo variable es: $${costos.costoVariable}</li>
+                        <li> La ganancia pretendida es: ${costos.ganancia}%</li></ul>
+                        <h6> El precio ideal es: <strong>$${precioVenta}</strong></h6>
+                        `;
+                        listaDatos.appendChild(div);
+    }) 
+    
+    //almacenamos los datos capturados
+    localStorage.setItem("ultimoDatoIngresado", JSON.stringify(costos))
+})
+
+
+//FUNCIONES PARA ELIMINAR LOS DATOS INGRESADOS
+
+const mostrarDatos = () => {
+    listaDatos.innerHTML = "";
+}
+
+const eliminarDatos = document.getElementById("eliminarDatos");
+
+eliminarDatos.addEventListener("click", () => {
+    vaciarDatos();
+})
+
+const vaciarDatos = () => {
+    datosIngresados = [];
+    mostrarDatos();
+
+    //Vaciar localStorage
+    localStorage.clear();
 }
 
 //funcion para calcular precio de venta 
@@ -40,52 +104,5 @@ function CalculoPrecioVenta (costoFijo, costoVariable, ganancia) {
     return (costoFijo + costoVariable) / ( 1 - ganancia/100);
 }
 
-//funcion para mostrar el resultado de la operacion
-function resultado (){
-    alert("El precio de venta de tu producto, segun los datos ingresados es: $" + precioVenta);
-}
-resultado ();
 
-//objeto y funcion constructora
-
-class Costos {
-    constructor (costoFijo, costoVariable, ganancia, precioVenta) {
-        this.costoFijo = costoFijo;
-        this.costoVariable = costoVariable;
-        this.ganancia = ganancia;
-        this.precioVenta = precioVenta
-    }
-    listaCostos() {
-        console.log("Estos son los datos ingresados: \n" 
-        + "Costo fijo: " + (this.costoFijo) + "\n" +
-        "Costo variable: " + (this.costoVariable) + "\n"
-        + "Ganancia pretendida: " + (this.ganancia) + "%" + "\n" 
-        + "El precio de tu producto es: $" + (this.precioVenta));
-    }
-}
-let costos1 = new Costos (costoFijo, costoVariable, ganancia, precioVenta);
-costos1.listaCostos();
-
-//array para almacenar datos
-
-const datosIngresados = [];
-
-datosIngresados.push(new Costos(costoFijo, costoVariable, ganancia, precioVenta));
-console.log(datosIngresados);
-
-
-
-const guardarResultado = datosIngresados.map((el) => el.precioVenta);
-console.log(guardarResultado);
-
-const contenedorDatos = document.getElementById("listaDatos");
-datosIngresados.forEach( costos => {
-    const div = document.createElement("div");
-    div.innerHTML = `   <h3> Los datos ingresados son: </h3>
-                        <ul><li> El costo fijo es: $${costos.costoFijo}</li>
-                        <li> El costo variable es: $${costos.costoVariable}</li>
-                        <li> La ganancia pretendida es: ${costos.ganancia}%</li></ul>
-                        <p> El precio ideal para su producto segun los datos recolectados es: $${costos.precioVenta}</p>`;
-                        listaDatos.appendChild(div);
-})  
 
